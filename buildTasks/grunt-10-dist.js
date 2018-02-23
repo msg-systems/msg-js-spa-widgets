0a45f24704fe94b28b9ab9d06755e4860103ad32
Jochen Hoertreiter <Jochen.Hoertreiter@msg-systems.com>
2018-02-23 16:38:36 +0100

    updated dependencies added eonasdan-bootstrap-datetimepicker that replaces the bootstrap-datepicker and bootstrap-timepicker (both remain for compatibility)

--- buildTasks/grunt-10-dist.js
+++ buildTasks/grunt-10-dist.js
@@ -11,26 +11,8 @@ module.exports = function (grunt) {
             "code": {
                 options: {
                     sourceMap: false,
-                    "plugins": [
-                        "transform-es2015-arrow-functions",
-                        "transform-es2015-block-scoped-functions",
-                        "transform-es2015-block-scoping",
-                        "transform-es2015-classes",
-                        "transform-es2015-computed-properties",
-                        "check-es2015-constants",
-                        "transform-es2015-destructuring",
-                        "transform-es2015-for-of",
-                        "transform-es2015-function-name",
-                        "transform-es2015-literals",
-                        "transform-es2015-object-super",
-                        "transform-es2015-parameters",
-                        "transform-es2015-shorthand-properties",
-                        "transform-es2015-spread",
-                        "transform-es2015-sticky-regex",
-                        "transform-es2015-template-literals",
-                        "transform-es2015-typeof-symbol",
-                        "transform-es2015-unicode-regex",
-                        "transform-regenerator"
+                    "presets": [
+                        "env"
                     ]
                 },
                 files: [{expand: true, cwd: 'tmp/app/', src: ['**/*.js'], dest: 'dist/app/'}]
